949bbb808a33accb369f3cf54fd13c313459c370
Neil Anderson <neil.anderson@icloud.com>
2020-11-27 19:20:57 +0000

    Initial commit

---
+++ test.js
@@ -0,0 +1,8 @@
+/**
+ * This is a bit of documentation for a JS function.
+ *
+ * @param {string} name Set the name for the greeting.
+ */
+function helloWorld(name) {
+	console.log(`Hello there, ${name}!`);
+}
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * This is a bit of documentation for a PHP function.
+ *
+ * @param {string} $name Set the name for the greeting.
+ */
+function helloWorld(string $name) {
+	echo "Hello there, {$name}!";
+}
